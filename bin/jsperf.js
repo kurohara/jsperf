421fea3bb7b3cf0bd067fb48ce6614082b2ff995
kurohara <kurohara@yk.rim.or.jp>
2015-10-22 17:13:37 +0900

    header comment added

--- bin/jsperf.js
+++ bin/jsperf.js
@@ -1,3 +1,10 @@
+/**
+ * jsperf cli.
+ * *** UNDER CONSTRUCTION, FOR BEHAVIOR TEST ***
+ * Uses mongodb as datastore, and monitors vm_stat command output.
+ *
+ * Copyright(C) 2015 Hiroyoshi Kurohara(Microgadget,inc.) all rights reserved.
+ */
 var Controller = require('../lib/perfcontroller');
 var monitor = require('jsperf.monitor.vm_stat');
 var Datastore = require('jsperf.datastore.mongo');
