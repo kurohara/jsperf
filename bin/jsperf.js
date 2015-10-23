59164cc20f4769de4e297ccd2a01e79d6d64dc00
kurohara <kurohara@yk.rim.or.jp>
2015-10-23 16:52:24 +0900

    command line arguments has added

--- bin/jsperf.js
+++ bin/jsperf.js
@@ -6,9 +6,29 @@
  * Copyright(C) 2015 Hiroyoshi Kurohara(Microgadget,inc.) all rights reserved.
  */
 var Controller = require('../lib/jsperf').Controller;
-var monitor = require('jsperf.monitor.vm_stat');
-var Datastore = require('jsperf.datastore.mongo');
 
-var controller = new Controller(monitor, new Datastore());
-controller.start();
+var program = require('commander');
 
+program.version('0.1.0')
+  .option('-m, --monitor [module]', 'Specify the name of monitor module', '')
+  .option('-d, --data [module]', 'Specify the name of datastore module', '')
+  .parse(process.argv);
+
+function load_module(prefix, modulename) {
+  if (modulename.indexOf(prefix) >= 0) {
+    return require(modulename);
+  } else {
+    return require(prefix + modulename);
+  }
+}
+
+if (program.monitor.length > 0 && program.data.length > 0) {
+  var monitor = load_module('jsperf.monitor.', program.monitor);
+  var Datastore = load_module('jsperf.datastore.', program.data);
+
+  var controller = new Controller(monitor, new Datastore());
+  controller.start();
+
+} else {
+  program.outputHelp();
+}
